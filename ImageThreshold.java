7f6b5dba56addc2856380ae08c880c0e5e252bb2
NicholasDelaney11 <34158163+NicholasDelaney11@users.noreply.github.com>
2019-03-08 20:32:33 -0330

    Add files via upload

--- ImageThreshold.java
+++ ImageThreshold.java
@@ -68,6 +68,32 @@ public class ImageThreshold extends Frame implements ActionListener {
         addWindowListener(new ExitListener());
         setSize(width * 2 + 400, height + 100);
         setVisible(true);
+        displayHistogram();
+    }
+
+    private void displayHistogram() {
+        int red = 0, green = 0, blue = 0;
+        int[] rH = new int[256];
+        int[] gH = new int[256];
+        int[] bH = new int[256];
+        // get histogram for rgb channels
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                Color clr = new Color(target.image.getRGB(x, y));
+                red = clr.getRed();
+                green = clr.getGreen();
+                blue = clr.getBlue();
+                rH[red]++;
+                gH[green]++;
+                bH[blue]++;
+            }
+        }
+        // plot histogram lines
+        for (int i = 1; i <= 255; i++) {
+            plot.drawLineSegment(Color.RED, i - 1, rH[i - 1] / 3, i, rH[i] / 3);
+            plot.drawLineSegment(Color.GREEN, i - 1, gH[i - 1] / 3, i, gH[i] / 3);
+            plot.drawLineSegment(Color.BLUE, i - 1, bH[i - 1] / 3, i, bH[i] / 3);
+        }
     }
 
     class ExitListener extends WindowAdapter {
@@ -82,18 +108,35 @@ public class ImageThreshold extends Frame implements ActionListener {
         // example -- compute the average color for the image
         if (((Button) e.getSource()).getLabel().equals("Adaptive Mean-C")) {
             //selects an individual threshold for each pixel based on the range of intensity values in its local neighborhood
-            int c = 7;
-            int kSize = c * c;
+            int c = 0;
+
             int w = 3;
-            for (int q = w; q < height - w; q++) {
-                for (int p = w; p < width - w; p++) {
+            int kSize = (2 * w + 1) * (2 * w + 1);
+            for (int q = 0; q < height; q++) {
+                for (int p = 0; p < width; p++) {
                     int rSum = 0;
                     int gSum = 0;
                     int bSum = 0;
                     for (int v = -w; v <= w; v++) {
                         for (int u = -v; u <= w; u++) {
-                            // [p + u] = x, [q + v] = y
-                            Color clr = new Color(source.image.getRGB(p + u, q + v));
+                            // border handling - if pixel is out of bounds use closest pixel
+                            int y = q + v;
+                            if (y < 0) {
+                                y = 0;
+                            }
+                            if (y > height - 1) {
+                                y = height - 1;
+                            }
+                            int x = p + u;
+                            if (x < 0) {
+                                x = 0;
+                            }
+                            if (x > width - 1) {
+                                x = width - 1;
+                            }
+
+                            // sum for each color channel in kernel
+                            Color clr = new Color(source.image.getRGB(x, y));
                             int red = clr.getRed();
                             int green = clr.getGreen();
                             int blue = clr.getBlue();
@@ -112,14 +155,11 @@ public class ImageThreshold extends Frame implements ActionListener {
                     red = (red < rT) ? 0 : GREY_LEVEL - 1;
                     green = (green < gT) ? 0 : GREY_LEVEL - 1;
                     blue = (blue < bT) ? 0 : GREY_LEVEL - 1;
-                    target.image.setRGB(p, q, red << 16 | green << 8 | blue);                    
+                    target.image.setRGB(p, q, red << 16 | green << 8 | blue);
                 }
             }
+            plot.clearObjects();
             target.repaint();
-        }
-
-        if (((Button) e.getSource()).getLabel().equals("Otsu's Method")) {
-
         }
 
         if (((Button) e.getSource()).getLabel().equals("Automatic Selection")) {
@@ -144,7 +184,8 @@ public class ImageThreshold extends Frame implements ActionListener {
             int bMean2;
 
             // Automatic threshold selection independently for each channel:
-            while (Math.abs(tR2 - tR) > 5 && Math.abs(tG2 - tG) > 5 && Math.abs(tB2 - tB) > 5) {
+            int delta = 1;
+            while (Math.abs(tR2 - tR) > delta && Math.abs(tG2 - tG) > delta && Math.abs(tB2 - tB) > delta) {
                 for (int y = 0; y < height; y++) {
                     for (int x = 0; x < width; x++) {
                         Color clr = new Color(source.image.getRGB(x, y));
@@ -220,7 +261,9 @@ public class ImageThreshold extends Frame implements ActionListener {
             }
             target.repaint();
             plot.clearObjects();
-            //plot.addObject(new VerticalBar(Color.BLACK, t, 100));
+            plot.addObject(new VerticalBar(Color.RED, tR, 100));
+            plot.addObject(new VerticalBar(Color.GREEN, tG, 100));
+            plot.addObject(new VerticalBar(Color.BLUE, tB, 100));
         }
 
         if (((Button) e.getSource()).getLabel().equals("Manual Selection")) {
@@ -248,9 +291,155 @@ public class ImageThreshold extends Frame implements ActionListener {
             }
             target.repaint();
         }
+
+        if (((Button) e.getSource()).getLabel().equals("Otsu's Method")) {
+            int ptr = 0;
+            int red = 0, green = 0, blue = 0, black = 0;
+
+            int[] rH = new int[256];
+            int[] gH = new int[256];
+            int[] bH = new int[256];
+
+            for (int y = 0, i = 0; y < height; y++) {
+                for (int x = 0; x < width; x++, i++) {
+                    Color clr = new Color(source.image.getRGB(x, y));
+                    red = clr.getRed();
+                    green = clr.getGreen();
+                    blue = clr.getBlue();
+                    rH[red]++;
+                    gH[green]++;
+                    bH[blue]++;
+                }
+            }
+
+            // Total number of pixels
+            int total = width * height;
+
+            float sumR = 0;
+            float sumG = 0;
+            float sumB = 0;
+            for (int t = 0; t < 256; t++) {
+
+                sumR += t * rH[t];
+                sumG += t * gH[t];
+                sumB += t * bH[t];
+            }
+
+            float sum2R = 0;
+            float sum2G = 0;
+            float sum2B = 0;
+            int wBackR = 0;
+            int wBackG = 0;
+            int wBackB = 0;
+            int wFR = 0;
+            int wFG = 0;
+            int wFB = 0;
+
+            float varMaxR = 0;
+            float varMaxG = 0;
+            float varMaxB = 0;
+
+            float thresholdR = 0;
+            float thresholdG = 0;
+            float thresholdB = 0;
+
+            for (int t = 0; t < 256; t++) {
+                // Weight Background
+                wBackR += rH[t];
+                wBackG += gH[t];
+                wBackB += bH[t];
+
+                // Weight Foreground
+                wFR = total - wBackR;
+                wFG = total - wBackG;
+                wFB = total - wBackB;
+                if (wFR == 0 || wFG == 0 || wFB == 0) {
+                    break;
+                }
+
+                sum2R += (float) (t * rH[t]);
+                sum2G += (float) (t * gH[t]);
+                sum2B += (float) (t * bH[t]);
+
+                // Mean Background
+                float mBackR = sum2R / wBackR;
+                float mBackG = sum2G / wBackG;
+                float mBackB = sum2B / wBackB;
+
+                // Mean Foreground
+                float mFR = (sumR - sum2R) / wFR;
+                float mFG = (sumG - sum2G) / wFG;
+                float mFB = (sumB - sum2B) / wFB;
+
+                // Calculate Between Class Variance
+                float varBetweenR = (float) wBackR * (float) wFR * (mBackR - mFR) * (mBackR - mFR);
+                float varBetweenG = (float) wBackG * (float) wFG * (mBackG - mFG) * (mBackG - mFG);
+                float varBetweenB = (float) wBackB * (float) wFB * (mBackB - mFB) * (mBackB - mFB);
+
+                if (varBetweenR > varMaxR) {
+                    varMaxR = varBetweenR;
+                    thresholdR = t;
+                }
+                if (varBetweenG > varMaxG) {
+                    varMaxG = varBetweenG;
+                    thresholdG = t;
+                }
+
+                if (varBetweenB > varMaxB) {
+                    varMaxB = varBetweenB;
+                    thresholdB = t;
+                }
+            }
+
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    Color clr = new Color(source.image.getRGB(x, y));
+                    int red2 = clr.getRed();
+                    int green2 = clr.getGreen();
+                    int blue2 = clr.getBlue();
+                    red = (red2 < thresholdR) ? 0 : GREY_LEVEL - 1;
+                    green = (green2 < thresholdG) ? 0 : GREY_LEVEL - 1;
+                    blue = (blue2 < thresholdB) ? 0 : GREY_LEVEL - 1;
+                    target.image.setRGB(x, y, red << 16 | green << 8 | blue);
+                }
+            }
+            
+            plot.clearObjects();
+            plot.addObject(new VerticalBar(Color.RED, (int) thresholdR, 100));
+            plot.addObject(new VerticalBar(Color.GREEN, (int) thresholdG, 100));
+            plot.addObject(new VerticalBar(Color.BLUE, (int) thresholdB, 100));
+            target.repaint();
+
+        }
+
+        if (((Button) e.getSource()).getLabel().equals("Display Histogram")) {
+            int red = 0, green = 0, blue = 0;
+            int[] rH = new int[256];
+            int[] gH = new int[256];
+            int[] bH = new int[256];
+            // get histogram for rgb channels
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    Color clr = new Color(target.image.getRGB(x, y));
+                    red = clr.getRed();
+                    green = clr.getGreen();
+                    blue = clr.getBlue();
+                    rH[red]++;
+                    gH[green]++;
+                    bH[blue]++;
+                }
+            }
+            // plot histogram lines
+            for (int i = 1; i <= 255; i++) {
+                plot.drawLineSegment(Color.RED, i - 1, rH[i - 1] / 3, i, rH[i] / 3);
+                plot.drawLineSegment(Color.GREEN, i - 1, gH[i - 1] / 3, i, gH[i] / 3);
+                plot.drawLineSegment(Color.BLUE, i - 1, bH[i - 1] / 3, i, bH[i] / 3);
+            }
+        }
+
     }
 
     public static void main(String[] args) {
-        new ImageThreshold(args.length == 1 ? args[0] : "fingerprint.png");
+        new ImageThreshold(args.length == 1 ? args[0] : "baboon.png");
     }
 }
